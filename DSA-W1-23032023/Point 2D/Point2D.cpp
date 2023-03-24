7796c6d83c3f6f8392d6eaf9f863d101e254accd
Terry <se.terry.2004@gmail.com>
2023-03-24 19:23:13 +0700

    Updated by sweterry

--- DSA-W1-23032023/Point 2D/Point2D.cpp
+++ DSA-W1-23032023/Point 2D/Point2D.cpp
@@ -1,59 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
 
 struct point
 {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 typedef struct point POINT;
 
-void Input(POINT[], int&);
-void PointQuickSort(POINT[], int, int);
-void Output(POINT[], int);
+bool PointCompare(const POINT& p1, const POINT& p2)
+{
+    return (p1.x != p2.x) ? p1.x < p2.x : p1.y > p2.y;
+}
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	static POINT AP[1000000];
-	int n;
-	Input(AP, n);
-    PointQuickSort(AP, 0, n - 1);
-	Output(AP, n);
-	return 0;
-}
-
-void Input(POINT AP[], int& n)
-{
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> AP[i].x >> AP[i].y;
-}
-void PointQuickSort(POINT AP[], int left, int right)
-{
-    int i = left, j = right;
-    POINT pivot = AP[(left + right) / 2];
-    while (i <= j) 
-    {
-        while (AP[i].x < pivot.x || (AP[i].x == pivot.x && AP[i].y > pivot.y))
-            i++;
-        while (AP[j].x > pivot.x || (AP[j].x == pivot.x && AP[j].y < pivot.y))
-            j--;
-        if (i <= j) 
-        {
-            swap(AP[i], AP[j]);
-            i++;
-            j--;
-        }
-    };
-    if (left < j)
-        PointQuickSort(AP, left, j);
-    if (i < right)
-        PointQuickSort(AP, i, right);
-}
-void Output(POINT AP[], int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << AP[i].x << " " << AP[i].y << endl;
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    vector <POINT> points(n);
+    for (auto& ap : points)
+        cin >> ap.x >> ap.y;
+    sort(points.begin(), points.end(), PointCompare);
+    for (const auto& ap : points)
+        cout << ap.x << " " << ap.y << "\n";
+    return 0;
 }
---
+++ DSA-W1-23032023/Rotation/Rotation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+void input(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
+
+void rotate(int a[], int n, int k) {
+    k %= n;
+    reverse(a, a + n);
+    reverse(a, a + k);
+    reverse(a + k, a + n);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    const int MAXN = 1e6;
+    int a[MAXN];
+    int n, k;
+    cin >> n >> k;
+    input(a, n);
+    rotate(a, n, k);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
